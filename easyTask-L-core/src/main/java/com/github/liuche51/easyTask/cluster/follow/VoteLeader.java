ac1572ec2e64b90e7d764675be7b989b3dfa7722
liuche <liuche51@foxmail.com>
2020-07-01 20:36:36 +0800

    选新leader优化流程。加入数据同步状态

--- easyTask-L-core/src/main/java/com/github/liuche51/easyTask/cluster/follow/VoteLeader.java
+++ easyTask-L-core/src/main/java/com/github/liuche51/easyTask/cluster/follow/VoteLeader.java
@@ -8,29 +8,42 @@ import com.github.liuche51.easyTask.dto.ScheduleBak;
 import com.github.liuche51.easyTask.dto.Task;
 import com.github.liuche51.easyTask.dto.zk.ZKHost;
 import com.github.liuche51.easyTask.dto.zk.ZKNode;
+import com.github.liuche51.easyTask.enume.NodeSyncDataStatusEnum;
 import org.apache.zookeeper.server.quorum.Leader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class VoteLeader {
     private static final Logger log = LoggerFactory.getLogger(VoteLeader.class);
     /**
      * 选举失效leader后新leader
      * 如果新leader是自己，则将备份数据重新提交给自己，否则不用管
-     * 选举规则是，所有follows中hashcode最小的
+     * 选举规则是，先比较数据同步状态为已同步的，然后选已同步状态的follows中hashcode最小的。否则就在未同步状态中选最小的
      * @param node
      * @param oldLeaderAddress
      */
     public static void selectNewLeader(ZKNode node,String oldLeaderAddress){
         List<ZKHost> follows = node.getFollows();
+        List<ZKHost> syncFollows=follows.stream().filter(x-> NodeSyncDataStatusEnum.SYNC==x.getDataStatus()).collect(Collectors.toList());
         ZKHost newLeader = null;
         int smallest = 0;
-        for (ZKHost x : follows) {
-            if (smallest == 0||x.getAddress().hashCode() < smallest) {
-                smallest = x.getAddress().hashCode();
-                newLeader = x;
+        if(syncFollows.size()>0){
+            for (ZKHost x : syncFollows) {
+                if (smallest == 0||x.getAddress().hashCode() < smallest) {
+                    smallest = x.getAddress().hashCode();
+                    newLeader = x;
+                }
+            }
+        }else {
+            log.error("selectNewLeader()-> has no datastatus=1 nodes");
+            for (ZKHost x : follows) {
+                if (smallest == 0||x.getAddress().hashCode() < smallest) {
+                    smallest = x.getAddress().hashCode();
+                    newLeader = x;
+                }
             }
         }
         //自己就是新leader
