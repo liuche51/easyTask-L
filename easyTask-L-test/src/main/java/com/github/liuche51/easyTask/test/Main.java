64b0ed367c52a8c49a469f941cc30c6f4c32836b
liuche <liuche51@foxmail.com>
2020-07-13 20:56:46 +0800

    编写测试工程

--- easyTask-L-test/src/main/java/com/github/liuche51/easyTask/test/Main.java
+++ easyTask-L-test/src/main/java/com/github/liuche51/easyTask/test/Main.java
@@ -5,130 +5,83 @@ import com.github.liuche51.easyTask.test.task.CusTask1;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.time.ZonedDateTime;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 
 public class Main {
     private static Logger log = LoggerFactory.getLogger(Main.class);
-    private static AnnularQueue annularQueue=AnnularQueue.getInstance();
-    public static void main(String[] args){
-        allcustomSimpleSetTest();
-    }
-    static void allcustomSimpleSetTest(){
-        EasyTaskConfig config=EasyTaskConfig.getInstance();
+
+    public static void main(String[] args) {
         try {
-            config.setTaskStorePath("C:\\db\\");
-            config.setSQLlitePoolSize(10);
-            config.setBackupCount(1);
-            annularQueue.setDispatchThreadPool( new ThreadPoolExecutor(4, 4, 1000, java.util.concurrent.TimeUnit.MILLISECONDS,
+            Properties properties = new Properties();
+            // 使用ClassLoader加载properties配置文件生成对应的输入流
+            InputStream in = Main.class.getClassLoader().getResourceAsStream("resources/sys.properties");
+            // 使用properties对象加载输入流
+            properties.load(in);
+            AnnularQueue annularQueue = AnnularQueue.getInstance();
+            EasyTaskConfig config = EasyTaskConfig.getInstance();
+            String path = properties.getProperty("taskStorePath");
+            String serverPort = properties.getProperty("serverPort");
+            String name = properties.getProperty("name");
+            config.setTaskStorePath(path);
+            config.setServerPort(Integer.valueOf(serverPort));
+            config.setSQLlitePoolSize(5);
+            config.setBackupCount(2);
+            //config.setDeleteZKTimeOunt(500);
+            //config.setSelectLeaderZKNodeTimeOunt(500);
+            annularQueue.setDispatchThreadPool(new ThreadPoolExecutor(4, 4, 1000, java.util.concurrent.TimeUnit.MILLISECONDS,
                     new LinkedBlockingQueue<Runnable>()));
-            annularQueue.setWorkerThreadPool( new ThreadPoolExecutor(4, 8, 1000, java.util.concurrent.TimeUnit.MILLISECONDS,
+            annularQueue.setWorkerThreadPool(new ThreadPoolExecutor(4, 8, 1000, java.util.concurrent.TimeUnit.MILLISECONDS,
                     new LinkedBlockingQueue<Runnable>()));
             annularQueue.start();
             CusTask1 task1 = new CusTask1();
-            task1.setEndTimestamp(ZonedDateTime.now().plusSeconds(10).toInstant().toEpochMilli());
-            Map<String,String> param=new HashMap<String,String>(){
+            task1.setEndTimestamp(ZonedDateTime.now().plusSeconds(10).toInstant().toEpochMilli());//10秒后执行
+            Map<String, String> param = new HashMap<String, String>() {
                 {
-                    put("name","刘彻");
-                    put("birthday","1988-1-1");
-                    put("age","25");
-                    put("threadid",String.valueOf(Thread.currentThread().getId()));
+                    put("name", name);
+                    put("birthday", "1988-1-1");
+                    put("age", "25");
+                    put("threadid", String.valueOf(Thread.currentThread().getId()));
                 }
             };
             task1.setParam(param);
             CusTask1 task2 = new CusTask1();
             task2.setPeriod(30);
-            task2.setEndTimestamp(ZonedDateTime.now().plusSeconds(-10).toInstant().toEpochMilli());
+            task2.setEndTimestamp(ZonedDateTime.now().plusSeconds(10).toInstant().toEpochMilli());
             task2.setTaskType(TaskType.PERIOD);
             task2.setUnit(TimeUnit.SECONDS);
-            Map<String,String> param2=new HashMap<String,String>(){
+            Map<String, String> param2 = new HashMap<String, String>() {
                 {
-                    put("name","Jack");
-                    put("birthday","1986-1-1");
-                    put("age","32");
-                    put("threadid",String.valueOf(Thread.currentThread().getId()));
+                    put("name", name);
+                    put("birthday", "1986-1-1");
+                    put("age", "32");
+                    put("threadid", String.valueOf(Thread.currentThread().getId()));
                 }
             };
             task2.setParam(param2);
-            //annularQueue.submit(task1);
-            //annularQueue.submit(task2);
+      /*  try {
+            annularQueue.submitAllowWait(task2);//单次提交测试
         } catch (Exception e) {
             e.printStackTrace();
-        }
-    }
-    static void highlyConcurrentTest(){
-        annularQueue.start();
-        for(int i=0;i<20;i++){
-            Thread th1=new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    for(int i=0;i<10;i++) {
-                        CusTask1 task1 = new CusTask1();
-                        task1.setEndTimestamp(ZonedDateTime.now().plusSeconds(10).toInstant().toEpochMilli());
-                        Map<String,String> param=new HashMap<String,String>(){
-                            {
-                                put("name","刘彻");
-                                put("birthday","1988-1-1");
-                                put("age","25");
-                                put("threadid",String.valueOf(Thread.currentThread().getId()));
-                            }
-                        };
-                        task1.setParam(param);
-                        try {
-                            AnnularQueue.getInstance().submit(task1);
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    }
+        }*/
+            //JUnit默认是非守护线程启动和Main方法不同。这里防止当前主线程退出导致子线程也退出了
+            while (true) {
+                Thread.sleep(1000);
+                try {
+                    annularQueue.submitAllowWait(task1);//多次提交测试
+                    annularQueue.submitAllowWait(task2);
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-            });
-            th1.start();
-        }
-        for(int i=0;i<20;i++){
-            Thread th2=new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    for(int i=0;i<10;i++) {
-                        CusTask1 task1 = new CusTask1();
-                        task1.setPeriod(30);
-                        task1.setTaskType(TaskType.PERIOD);
-                        task1.setUnit(TimeUnit.SECONDS);
-                        task1.setEndTimestamp(ZonedDateTime.now().plusSeconds(10).toInstant().toEpochMilli());
-                        Map<String,String> param=new HashMap<String,String>(){
-                            {
-                                put("name","王林");
-                                put("birthday","1986-1-1");
-                                put("age","35");
-                                put("threadid",String.valueOf(Thread.currentThread().getId()));
-                            }
-                        };
-                        task1.setParam(param);
-                        try {
-                            AnnularQueue.getInstance().submit(task1);
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-            });
-            th2.start();
-        }
-        while (true){
-            try {
-                Thread.sleep(1000l);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
             }
-            log.debug("getDBScheduleCount="+Monitor.getDBScheduleCount());
-            log.debug("getDispatchsPoolWaiteToExecuteScheduleCount="+Monitor.getDispatchsPoolWaiteToExecuteScheduleCount());
-            log.debug("getWorkersPoolWaiteToExecuteScheduleCount="+Monitor.getWorkersPoolWaiteToExecuteScheduleCount());
-            log.debug("getScheduleInAnnularQueueCount="+Monitor.getScheduleInAnnularQueueCount());
-            log.debug("getDispatchsPoolExecutedScheduleCount="+Monitor.getDispatchsPoolExecutedScheduleCount());
-            log.debug("getWorkersPoolExecutedScheduleCount="+Monitor.getWorkersPoolExecutedScheduleCount());
-
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
